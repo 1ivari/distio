8a70a833f26a5d962e00b7c522b2c7f69e384575
1ivari <60034643+1ivari@users.noreply.github.com>
2022-11-16 14:55:47 +0200

    daisyui and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#2573d9",
+          
+					"secondary": "#b6d0f2",
+										
+					"accent": "#5d5d5d",
+										
+					"neutral": "#d9d9d9",
+										
+					"base-100": "#ffffff",
+										
+					"info": "#5FAAEC",
+										
+					"success": "#85E0BD",
+										
+					"warning": "#E7BF23",
+										
+					"error": "#EA6264",
+				},
+			},
+		],
+	},
+	plugins: [require('daisyui')],
 }
