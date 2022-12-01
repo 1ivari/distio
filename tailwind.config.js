6cd7602e77bb1171b957b974739a642d340306c7
1ivari <60034643+1ivari@users.noreply.github.com>
2022-12-01 12:24:40 +0200

    first navbar draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['*.{html,js}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'darkBlue': '#2573d9',
+        'lightBlue': '#b6d0f2',
+        'yellow': '#da9e00',
+        'grey': '#5d5d5d',
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
 }
